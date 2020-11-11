4cb2cf7483ed4d15eef7e933f3e2cc28698c1a77
Himanshu Koyalkar <himanshu.koyalkar@nike.com>
2020-11-11 02:42:26 -0800

    added further comments

--- server.js
+++ server.js
@@ -1,16 +1,20 @@
+//First file to be executed
+//Triggered when app(via Server) is started, server start command in package.json line 7
+
 const express = require('express');
 const expressGraphQL = require('express-graphql');
+
+//require schema as we will use schema for running query
 const schema = require('./schema')
 
 const app = express();
+//exposing app at port 4000
 const PORT = 4000;
 
 app.use('/graphql', expressGraphQL({
     graphiql: true,
     schema: schema
 }));
-
-
 
 app.listen(PORT, () => {
     console.log("Listening on port: "+PORT);
